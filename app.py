5a48f4d29aee1acfd2db577a5413c1d7ad670e09
Eugenia/Yujia Cao <eugenia.cao0804@gmail.com>
2025-06-06 13:50:39 -0500

    Seperate streamlit control functions

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 from utils.utils_data import *
 from utils.utils_models import *
 from utils.utils_vis import *
+from utils.utils_control import *
 from utils.utils_css import style
 import time
 
@@ -23,26 +24,26 @@ st.sidebar.header("Controls")
 
 # Filter data by location
 start_time = time.time()
-location_code = st.sidebar.multiselect("Select Location Code", options=["2", "9", "15"], default=["2", "9", "15"], key="location_code_selection")
+location_code = location_code_control()
 sales_df = filter_location(sales_df, location_code)
 log_timing("Data filtered", start_time, time.time())
 
 # Filter data by shipment method
 start_time = time.time()
-shipment_method = st.sidebar.multiselect("Select Shipment Method", options=["WILL CALL", "UPS GROUND"], default=["WILL CALL", "UPS GROUND"], key="shipment_method_selection")
+shipment_method = shipment_method_control()
 sales_df = filter_shipment_method(sales_df, shipment_method)
 log_timing("Data filtered", start_time, time.time())
 
 # Determine demand type
 start_time = time.time()
-quantity_or_sales = st.sidebar.radio("Select Quantity or Total Sales", options=["QUANTITY", "TOTAL_SALES"], index=0, key="quantity_or_sales_choice")
+quantity_or_sales = display_value_control()
 demand_type_info = determine_demand_type(sales_df, quantity_or_sales=quantity_or_sales)
 log_timing("Demand type determined", start_time, time.time())
 
 # Select SKU
 sku_list = sales_df['PRODUCT'].unique()
 sku_display_names, sku_name_mapping = get_display_name(type="sku", name_list=sku_list, demand_type_info=demand_type_info)
-selected_display = st.sidebar.selectbox("Select a SKU", sku_display_names, index=0, key="sku_selection")
+selected_display = sku_control(sku_display_names)
 selected_sku = sku_name_mapping[selected_display]
 
 # Product information
@@ -58,12 +59,13 @@ product_weekly = aggregate_weekly_sales(selected_sku, sales_df, quantity_or_sale
 
 # Split data
 min_date, max_date, default_date = get_split_dates(product_weekly)
-split_date = st.sidebar.slider("Select Train/Test Split Date:", min_value=min_date, max_value=max_date, value=default_date, format="YYYY-MM-DD", key="split_date_slider")
+split_date = split_date_control(min_date, max_date, default_date)
 train, test = train_test_split(product_weekly, split_date=split_date)
 
 # Check if enough sales records exist
 if (train != 0).sum() < 13:
-    st.warning("This product does not have enough sales records at this location! Please select more locations or choose a later split date.")
+    st.warning("This product does not have enough sales records at this location! " \
+                "Please select more locations or choose a later split date.")
     st.stop()
 
 # Forecasting
@@ -91,7 +93,7 @@ best_model_forecast = forecasts[best_model_name]
 
 # Model selection
 model_display_names, model_name_mapping = get_display_name(type="model", name_list=model_options, best_model_name=best_model_name)
-selected_display_name = st.sidebar.radio("Select a Forecasting Model:", model_display_names, index=model_display_names.index(best_model_name + " (Best Model)"), key="model_selection")
+selected_display_name = model_control(model_display_names, best_model_name)
 selected_model_name = model_name_mapping[selected_display_name]
 selected_model = model_options[model_name_mapping[selected_display_name]]
 
--- utils/utils_control.py
+++ utils/utils_control.py
@@ -1,23 +1,45 @@
 import streamlit as st
 
+def location_code_control():
+    selected_locations = st.sidebar.multiselect("Select Location Code", 
+                           options=["2", "9", "15"], 
+                           default=["2", "9", "15"], 
+                           key="location_code_selection")
+    return selected_locations
 
-quantity_or_sales = st.sidebar.radio(
-    "Select Quantity or Total Sales",
-    options=["QUANTITY", "TOTAL_SALES"],
-    index=0,
-    key="quantity_or_sales_choice"
-)
+def shipment_method_control():
+    selected_shipments = st.sidebar.multiselect("Select Shipment Method", 
+                                                options=["WILL CALL", "UPS GROUND"], 
+                                                default=["WILL CALL", "UPS GROUND"], 
+                                                key="shipment_method_selection")
+    return selected_shipments
 
-location_code = st.sidebar.multiselect(
-    "Select Location Code",
-    options=["2", "9", "15"],
-    default=["2", "9", "15"],
-    key="location_code_selection"
-)
+def display_value_control():
+    selected_display = st.sidebar.radio("Select Quantity or Total Sales", 
+                                        options=["QUANTITY", "TOTAL_SALES"], 
+                                        index=0, 
+                                        key="quantity_or_sales_choice")
+    return selected_display
 
-shipment_method = st.sidebar.multiselect(
-    "Select Shipment Method",
-    options=["WILL CALL", "UPS GROUND"],
-    default=["WILL CALL", "UPS GROUND"],
-    key="shipment_method_selection"
-)
+def sku_control(sku_display_names):
+    selected_sku = st.sidebar.selectbox("Select a SKU", 
+                                        sku_display_names, 
+                                        index=0, 
+                                        key="sku_selection")
+    return selected_sku
+
+def split_date_control(min_date, max_date, default_date):
+    selected_split_date = st.sidebar.slider("Select Train/Test Split Date:", 
+                                            min_value=min_date, 
+                                            max_value=max_date, 
+                                            value=default_date, 
+                                            format="YYYY-MM-DD", 
+                                            key="split_date_slider")
+    return selected_split_date
+
+def model_control(model_display_names, best_model_name):
+    selected_model = st.sidebar.radio("Select a Forecasting Model:", 
+                                      model_display_names, 
+                                      index=model_display_names.index(best_model_name + " (Best Model)"), 
+                                      key="model_selection")
+    return selected_model
